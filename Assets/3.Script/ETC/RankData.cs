d96cef1ebb113bb987b2b78689c7f976b5ea8bf3
최영훈 <thiore94@gmail.com>
2024-07-01 13:21:21 +0900

    [feat]Rank

---
+++ Assets/3.Script/ETC/RankData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankData 
+{
+    public int Rank;
+    public string name;
+    public int score;
+}
--- Assets/3.Script/ETC/RankingManager.cs
+++ Assets/3.Script/ETC/RankingManager.cs
@@ -1,25 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class RankingManager : MonoBehaviour
 {
-    public string name;
-    public int Score = 0;
+    public static RankingManager instance = null;
 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public List<RankData> ranks = new List<RankData>();
+    private RankData rank = new RankData();
     public void SortRank()
     {
 
+        
     }
     public void SaveRank()
     {
-        name = GameManager.instance.SelectPlayer.ToString();
-        Score = GameManager.instance.Score;
+        if (ranks.Count > 3)
+            ranks.RemoveAt(ranks.Count - 1);
+
+        rank.name = GameManager.instance.SelectPlayer.ToString();
+        rank.score = GameManager.instance.Score;
+        ranks.Add(rank);
+        string jsonData = JsonUtility.ToJson(ranks);
+        string path = Path.Combine(Application.dataPath, "rankData.json");
+        
+        File.WriteAllText(path, jsonData);
     }
 
     public void LoadRank()
     {
-
+        string path = Path.Combine(Application.dataPath, "rankData.json");
+        string jsonData = File.ReadAllText(path);
+        ranks = JsonUtility.FromJson<List<RankData>>(jsonData);
     }
 }
+
 
